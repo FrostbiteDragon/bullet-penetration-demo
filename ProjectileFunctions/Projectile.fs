2dfe3954f5c9fcf085d6c31ca0ca9620217ed25b
Cameron <cameronproulx@outlook.com>
2020-10-23 14:03:53 -0400

    code cleanup

--- ProjectileFunctions/Projectile.fs
+++ ProjectileFunctions/Projectile.fs
@@ -18,50 +18,50 @@ module Projectile =
               startPosition.z + startDirection.z * time * speed )
 
     let GetPenetrations (points : Vector3[]) = 
-                let forward =
+        let validate (forward : RaycastHit[]) (backwards : RaycastHit[]) =
+            match forward.Length = backwards.Length with
+            |true -> backwards
+            |false ->
+                let nextBackwards =
                     Physics.RaycastAll(
-                        points.[0],
-                        points.[1] - points.[0],
-                        Vector3.Distance(points.[0], points.[1]))
+                        points.[2],
+                        points.[1] - points.[2],
+                        Vector3.Distance(points.[1], points.[2]))
                     |> Array.sortBy(fun x -> x.distance)
-    
-                let backwards = 
-                    Physics.RaycastAll(
-                        points.[1],
-                        points.[0] - points.[1],
-                        Vector3.Distance(points.[0], points.[1]))
-                    |> Array.sortBy(fun x -> x.distance)
-
-                let backwards =
-                    if not(forward.Length = backwards.Length) then
-                        let nextBackwards =
-                            Physics.RaycastAll(
-                                points.[2],
-                                points.[1] - points.[2],
-                                Vector3.Distance(points.[1], points.[2]))
-                            |> Array.sortBy(fun x -> x.distance)
                     
-                        if nextBackwards.Length > 0 && forward.[forward.Length - 1].collider = nextBackwards.[nextBackwards.Length - 1].collider then
-                            backwards
+                match nextBackwards.Length > 0 && forward.[forward.Length - 1].collider = nextBackwards.[nextBackwards.Length - 1].collider with
+                |true -> backwards
                             |> List.ofArray
                             |> List.append [nextBackwards.[nextBackwards.Length - 1]]
                             |> Array.ofList
-                        else
-                            backwards
-                    else
-                        backwards
+                | false -> backwards
 
-                let extractPenetrationResults (entry : RaycastHit) (exit : Option<RaycastHit>) =
-                    let thickness =
-                        match exit with
-                        |Some exit -> Vector3.Distance(entry.point, exit.point) * 1000.0f
-                        |None -> Single.PositiveInfinity
-
-                    { thickness = thickness
-                      entryHit = entry
-                      exitHit = exit |> Option.toNullable }
+        let forward =
+            Physics.RaycastAll(
+                points.[0],
+                points.[1] - points.[0],
+                Vector3.Distance(points.[0], points.[1]))
+            |> Array.sortBy(fun x -> x.distance)
     
-                forward
-                |> Array.mapi (fun i x -> extractPenetrationResults x (backwards |> Array.tryItem (backwards.Length - i - 1)))
+        let backwards = 
+            Physics.RaycastAll(
+                points.[1],
+                points.[0] - points.[1],
+                Vector3.Distance(points.[0], points.[1]))
+            |> Array.sortBy(fun x -> x.distance)
+            |> validate forward
+
+        let extractPenetrationResults (entry : RaycastHit) (exit : Option<RaycastHit>) =
+            let thickness =
+                match exit with
+                |Some exit -> Vector3.Distance(entry.point, exit.point) * 1000.0f
+                |None -> Single.PositiveInfinity
+
+            { thickness = thickness
+              entryHit = entry
+              exitHit = exit |> Option.toNullable }
+    
+        forward
+        |> Array.mapi (fun i x -> extractPenetrationResults x (backwards |> Array.tryItem (backwards.Length - i - 1)))
 
 
